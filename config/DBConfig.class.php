e3b8ed01745757ce96105b94e8f19263df025493
samuelfilipefaria <samuelfilipefaria@gmail.com>
2023-11-27 20:37:18 -0300

    Creating config files

---
+++ config/DBConfig.class.php
@@ -0,0 +1,8 @@
+<?php
+  class DBConfig {
+    static $HOST = "";
+    static $USER = "";
+    static $PWD = "";
+    static $DB = "";
+  }
+?>
---
+++ config/GlobalConfig.class.php
@@ -0,0 +1,5 @@
+<?php
+  class GlobalConfig {
+    static $ROOT_DIR = "/";
+  }
+?>
